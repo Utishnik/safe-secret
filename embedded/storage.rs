8231720208099cd85d07fcfa9aa2b0afa4590f0c
Utishnik <yrik.kokoyto.00@bk.ru>
2026-02-07 02:00:11 +0300

    add FileCrypt methods

--- embedded/storage.rs
+++ embedded/storage.rs
@@ -1,17 +1,55 @@
+use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
 
-pub struct FileCrypt<'key,'path>{
+const DEFAULT_MIN_DEPTH: usize = 0;
+const DEFAULT_MAX_DEPTH: usize = 2;
+
+pub struct FileCrypt<'key, 'path> {
+    crypt_data: Vec<u8>,
+    decrypt_data: Vec<u8>,
     key: Option<&'key str>,
     path: &'path Path,
 }
 
-pub struct DirCrypt<'key,'path>{
+impl FileCrypt<'_, '_> {
+    pub fn set_crypt_data_file(&mut self) {
+        let get_bytes_crypt_data: Result<Vec<u8>, std::io::Error> = load_crypt_file(self.path);
+        if get_bytes_crypt_data.is_err() {
+            eprintln!("set_crypt_data_file is err!");
+            return;
+        }
+        self.crypt_data = get_bytes_crypt_data.unwrap();
+    }
+
+    pub fn get_crypt_data_file_borrow(&self) -> &Vec<u8> {
+        &self.crypt_data
+    }
+
+    pub fn clone_crypt_data_file(&mut self) -> Vec<u8> {
+        self.crypt_data.clone()
+    }
+
+    pub fn move_crypt_data_file(self) -> Vec<u8> {
+        self.crypt_data
+    }
+
+    pub fn get_decrypt_data_file_borrow(&self) -> &Vec<u8> {
+        &self.decrypt_data
+    }
+
+    pub fn clone_decrypt_data_file(&mut self) -> Vec<u8> {
+        self.decrypt_data.clone()
+    }
+}
+
+pub struct DirCrypt<'key, 'path> {
+    data: Vec<FileCrypt<'key, 'path>>,
     key: Option<&'key str>,
     path: &'path Path,
 }
 
-fn list_all_files(dir: &Path,min_depth: usize,max_depth: usize) -> Vec<String> {
+fn list_all_files(dir: &Path, min_depth: usize, max_depth: usize) -> Vec<String> {
     WalkDir::new(dir)
         .min_depth(min_depth)
         .max_depth(max_depth)
@@ -22,14 +60,21 @@ fn list_all_files(dir: &Path,min_depth: usize,max_depth: usize) -> Vec<String> {
         .collect()
 }
 
-fn list_all_files_def(dir: &Path) -> Vec<String>{
-    list_all_files(dir, 0, 2)
+fn list_all_files_def(dir: &Path) -> Vec<String> {
+    list_all_files(dir, DEFAULT_MIN_DEPTH, DEFAULT_MAX_DEPTH)
 }
 
-pub fn secrets_dir_get(path: &Path) -> Option<Vec<String>>{
+pub fn secrets_dir_get(path: &Path) -> Option<Vec<String>> {
     let files: Vec<String> = list_all_files_def(path);
-    if files.is_empty(){
+    if files.is_empty() {
         return None;
     }
     Some(files)
 }
+
+fn load_crypt_file(path: &Path) -> std::io::Result<Vec<u8>> {
+    let bytes: Vec<u8> = fs::read(path)?;
+    Ok(bytes)
+}
+
+pub fn load_crypt_dir(path: &Path) {}
